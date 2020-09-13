d2a14e0bbd18dd2ee4f3a0d729f3ebf776739aea
Bogdan <mormul96@ukr.net>
2020-09-13 13:50:23 +0300

    Updates

---
+++ precache-manifest.750725b0e30bdfb1297f8df18cde1583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a07b0db12f6007a60022de33dd11111",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "a13635c6a99b32770a4c",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.0fda2c40.chunk.css"
+  },
+  {
+    "revision": "aa3fd4bab2a63f599d13",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.5015fe69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.5015fe69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13635c6a99b32770a4c",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.74008c6c.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
